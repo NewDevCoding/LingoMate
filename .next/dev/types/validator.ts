15ab095dedfad7984294e065d239f52132c42e54
Isaac <newdevcoding@gmail.com>
2026-01-31 12:44:29 -0500

    added vocab adding feature

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -173,6 +173,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../../app/api/translation/route.ts
+{
+  type __IsExpected<Specific extends RouteHandlerConfig<"/api/translation">> = Specific
+  const handler = {} as typeof import("../../../app/api/translation/route.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/api/vocabulary/route.ts
 {
   type __IsExpected<Specific extends RouteHandlerConfig<"/api/vocabulary">> = Specific
